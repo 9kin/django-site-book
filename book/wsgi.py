215319d69e9d8293937bd052031f4c509d94f5fb
9kin <gate.web.tester@mail.ru>
2019-03-12 21:39:33 +0500

    pythonanywhere3?

--- book/wsgi.py
+++ book/wsgi.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 # assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/myusername/mysite'
+path = '/home/9kin/book'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'book.settings'
 
 ## Uncomment the lines below depending on your Django version
 ###### then, for Django >=1.5:
