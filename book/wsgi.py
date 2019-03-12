cfe55073126e4acb310a297adcc1590a1bd56fa8
9kin <gate.web.tester@mail.ru>
2019-03-12 21:20:28 +0500

    pythonanywhere

--- book/wsgi.py
+++ book/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for book project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
+#http://help.pythonanywhere.com/pages/FollowingTheDjangoTutorial
 import os
+import sys
 
+path = os.path.expanduser('~/book')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'book.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
